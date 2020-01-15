0d55a1542194c8510e4dd36627b2c382f742458a
nguyenquangthang1997 <nguyenquangthang1997@gmail.com>
2020-01-15 20:08:27 +0700

    fix visualize

--- test1.py
+++ test1.py
@@ -14,20 +14,20 @@ for line in data:
 
 
 
-# data = open("/home/mpiuser/cloud/result.txt", "r")
+data = open("/home/mpiuser/cloud/result.txt", "r")
+
+# data_line = data.readline()
 #
-# # data_line = data.readline()
-#
-# point7 = []
-# point8 = []
-# temp = []
-# for line in data:
-#     data_string = line.split(" ")
-#     point7.append(int(data_string[0]))
-#     point8.append(int(data_string[1][:len(data_string[1]) - 1]))
-#
+point7 = []
+point8 = []
+temp = []
+for line in data:
+    data_string = line.split(" ")
+    point7.append(int(data_string[0]))
+    point8.append(int(data_string[1][:len(data_string[1]) - 1]))
+pl.plot(point7, point8)
+
 
 pl.plot(point1, point2, "go")
 
-# pl.plot(point7, point8)
 pl.show()
